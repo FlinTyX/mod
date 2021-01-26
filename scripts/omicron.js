ed0b34aa760386f9277cf41ac86de749d320b91c
Flin <69006175+FlinTyX@users.noreply.github.com>
2021-01-25 21:05:03 -0300

    Create omicron.js

---
+++ scripts/omicron.js
@@ -0,0 +1,16 @@
+/*
+yey blue atmosphere serpulo. I will not do a new planet generator, it's very complex, i'll just wait v7
+*/
+
+const omicron = extend(Planet, "omicron", Planets.sun, 3, 1, {
+    generator: new SerpuloPlanetGenerator(),
+    bloom: true,
+    radius: 1,
+    accessible: true,
+    hasAtmosphere: true,
+    atmosphereColor: Color.valueOf("094985"),
+    atmosphereRadIn: 0.02,
+    atmosphereRadOut: 0.3,
+    localizedName: "Omicron"
+});
+omicron.meshLoader = () => extend(HexMesh, omicron, 6, {});
